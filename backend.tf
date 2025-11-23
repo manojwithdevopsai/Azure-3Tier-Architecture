81f9b966c628c11e909df540859da9a86fa2e524
Manoj Savukar <92372003+manojwithdevopsai@users.noreply.github.com>
2025-11-23 15:43:59 +0530

    Fix quotes in backend.tf configuration
    Added quotes around backend configuration values.

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = demo-terraform-rg
-    storage_account_name = demostorage34324
-    container_name       = tfstate
+    resource_group_name  = "demo-terraform-rg"
+    storage_account_name = "demostorage34324"
+    container_name       = "tfstate"
     key                  = "3tier/dev.terraform.tfstate"
   }
 }
