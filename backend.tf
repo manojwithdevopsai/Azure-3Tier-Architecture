6b5f20fd8cc5f3e37443684b9d486ce90af6a7f2
root <root@DESKTOP-KNL45VQ.>
2025-11-23 16:54:33 +0000

    updating environment variabe

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-terraform-rg"
-    storage_account_name = "demostorage34324"
+    resource_group_name  = "demorg9834"
+    storage_account_name = "demostorage9834"
     container_name       = "tfstate"
     key                  = "3tier/dev.terraform.tfstate"
   }
