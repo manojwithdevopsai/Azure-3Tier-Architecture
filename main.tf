0765beb9a30c40ec22bd40d4dbb0f13098d39734
Manoj Savukar <92372003+manojwithdevopsai@users.noreply.github.com>
2025-11-23 15:08:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ module "nsg" {
 }
 
 data "azurerm_key_vault" "kv" {
-  name                = ${var.kv_name}
-  resource_group_name = ${var.kv_resource_group_name}
+  name                = var.kv_name
+  resource_group_name = var.kv_resource_group_name
 }
 
 data "azurerm_key_vault_secret" "vm_username" {
