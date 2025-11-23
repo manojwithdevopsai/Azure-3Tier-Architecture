b41f4135fe85999f95880b2ed9108de07e88ad88
Manoj Savukar <92372003+manojwithdevopsai@users.noreply.github.com>
2025-11-23 15:42:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.kv_resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
+    resource_group_name  = demo-terraform-rg
+    storage_account_name = demostorage34324
+    container_name       = tfstate
     key                  = "3tier/dev.terraform.tfstate"
   }
 }
